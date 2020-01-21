795296cae0f24baf846b24d3182d4a84b2d28beb
ryancj1993 <ryancj_1993@icloud.com>
2020-01-21 01:17:06 -0600

    Initial file list

---
+++ R/myf.R
@@ -0,0 +1,14 @@
+#' Cube function
+#'
+#' Takes a vector, cubes it, and returns the new vector
+#'
+#' @param x
+#'
+#' @return a vector of cubed components
+#' @export
+#'
+#' @examples
+#' x=1:30; myf(x)
+myf=function(x){
+  x^3;
+}
